15748913086d7780b9e4c9b3f980c3e95a08c700
Nicolò Cavalieri <nicolo.cavalieri@studenti.unitn.it>
2024-05-16 10:30:21 +0200

    Added basic projects folder in C language

---
+++ basicsCprojects/calculator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main()
+{
+    // TO DO
+    // develop the case of a very high number of operations for all operatos (i.e 3 + 45 + 43+ 5 + ....)
+    
+    char operation;
+    double num1, num2, result;
+
+    //Prompt the user for some inputs
+    printf("Scientific calculator \n");
+    printf("Enter operator (+, -, *, /, ^): ");
+    scanf("%c", &operation);
+
+    // prompt user for numbers
+    printf("Insert 2 numbers (if you want to elevate 1 number to another 1 choose the 2 input as integer): ");
+    scanf("%lf %lf", &num1, &num2);
+
+
+    //Perform calculation based on the operation
+    switch(operation)
+    {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0) // division by 0!
+                result = num1 / num2;
+            else
+            {
+                printf("Error: Division by 0! \n");
+                return 1;
+            }          
+            break;
+        case '^':
+            result = 1;
+            // assert(num2 == (int)num2);
+            for(int i=1; i<=(int)num2; i++)
+                result *= num1;
+            break;
+        default:
+            printf("Error: invalid operation required! \n");
+            return 1;
+    }
+    // display result
+    printf("Result: %.5lf", result);
+    
+    return 0;
+}
